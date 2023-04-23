61332b9f5044696176f04e7444733cae2f3188ba
YTDark <YTDark@gmail.com>
2023-04-23 04:05:54 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,300 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.txtFilePath = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtInterval = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.lblRunTime = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtPyFile = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblSecond = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtPythonFile = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'txtFilePath
+        '
+        Me.txtFilePath.Location = New System.Drawing.Point(110, 11)
+        Me.txtFilePath.Name = "txtFilePath"
+        Me.txtFilePath.Size = New System.Drawing.Size(264, 20)
+        Me.txtFilePath.TabIndex = 0
+        Me.txtFilePath.Text = "C:\Program Files (x86)\Python310-32\python.exe"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(24, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Path File to Kill :"
+        '
+        'txtInterval
+        '
+        Me.txtInterval.Location = New System.Drawing.Point(110, 122)
+        Me.txtInterval.Name = "txtInterval"
+        Me.txtInterval.Size = New System.Drawing.Size(44, 20)
+        Me.txtInterval.TabIndex = 0
+        Me.txtInterval.Text = "30"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(58, 126)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Interval :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(304, 126)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(70, 13)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "RuningTime :"
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(12, 180)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(75, 23)
+        Me.btnStart.TabIndex = 3
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.Enabled = False
+        Me.btnStop.Location = New System.Drawing.Point(358, 180)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(75, 23)
+        Me.btnStop.TabIndex = 4
+        Me.btnStop.Text = "Stop"
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'lblRunTime
+        '
+        Me.lblRunTime.AutoSize = True
+        Me.lblRunTime.Location = New System.Drawing.Point(380, 126)
+        Me.lblRunTime.Name = "lblRunTime"
+        Me.lblRunTime.Size = New System.Drawing.Size(13, 13)
+        Me.lblRunTime.TabIndex = 5
+        Me.lblRunTime.Text = "0"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(17, 52)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(89, 13)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "Path File to Run :"
+        '
+        'txtPyFile
+        '
+        Me.txtPyFile.Location = New System.Drawing.Point(110, 48)
+        Me.txtPyFile.Name = "txtPyFile"
+        Me.txtPyFile.Size = New System.Drawing.Size(264, 20)
+        Me.txtPyFile.TabIndex = 6
+        Me.txtPyFile.Text = "C:\Users\Admin\Desktop\Contol\Contol_3_Token.py"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(6, 153)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(100, 13)
+        Me.Label7.TabIndex = 8
+        Me.Label7.Text = "Time to Sart Again :"
+        '
+        'lblSecond
+        '
+        Me.lblSecond.AutoSize = True
+        Me.lblSecond.Location = New System.Drawing.Point(110, 153)
+        Me.lblSecond.Name = "lblSecond"
+        Me.lblSecond.Size = New System.Drawing.Size(13, 13)
+        Me.lblSecond.TabIndex = 9
+        Me.lblSecond.Text = "0"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(35, 95)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(71, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Python Path :"
+        '
+        'txtPythonFile
+        '
+        Me.txtPythonFile.Location = New System.Drawing.Point(110, 91)
+        Me.txtPythonFile.Name = "txtPythonFile"
+        Me.txtPythonFile.Size = New System.Drawing.Size(264, 20)
+        Me.txtPythonFile.TabIndex = 10
+        Me.txtPythonFile.Text = "C:\Program Files (x86)\Python310-32\python.exe"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(380, 89)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(53, 23)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "Browse"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(380, 46)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(53, 23)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "Browse"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(380, 9)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(53, 23)
+        Me.Button3.TabIndex = 14
+        Me.Button3.Text = "Browse"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(160, 126)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(49, 13)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "Seconds"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(229, 125)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(63, 17)
+        Me.CheckBox1.TabIndex = 16
+        Me.CheckBox1.Text = "Minutes"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(160, 153)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(16, 13)
+        Me.lblStatus.TabIndex = 17
+        Me.lblStatus.Text = "..."
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(187, 180)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 18
+        Me.Button4.Text = "Kill"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(448, 210)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtPythonFile)
+        Me.Controls.Add(Me.lblSecond)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtPyFile)
+        Me.Controls.Add(Me.lblRunTime)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtInterval)
+        Me.Controls.Add(Me.txtFilePath)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Auto Kill-Run Python File 4.1"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents txtFilePath As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtInterval As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnStart As Button
+    Friend WithEvents btnStop As Button
+    Friend WithEvents lblRunTime As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtPyFile As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents lblSecond As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtPythonFile As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents Button4 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,165 @@
+﻿Imports System.Management
+Imports System.Threading
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Public th As Thread
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        CheckForIllegalCrossThreadCalls = False
+
+
+        If CheckBox1.Checked = True Then
+            lblSecond.Text = txtInterval.Text * 60
+        Else
+            lblSecond.Text = txtInterval.Text * 1
+
+        End If
+
+        btnStart.Enabled = False
+        btnStop.Enabled = True
+        txtInterval.Enabled = False
+        CheckBox1.Enabled = False
+
+        th = New Thread(AddressOf TimerCount)
+        th.Start()
+
+    End Sub
+
+    Sub RunPy()
+
+
+
+        Dim pythonPath As String = txtPythonFile.Text ' مسار تثبيت Python على جهاز الكمبيوتر الخاص بك
+        Dim scriptPath As String = txtPyFile.Text ' مسار ملف النص الخاص بـ Python
+
+        Dim processX As New Process()
+        processX.StartInfo.FileName = pythonPath
+        processX.StartInfo.Arguments = scriptPath
+        processX.Start()
+        lblStatus.Text = "Process Runing"
+
+    End Sub
+
+    Private Sub KillProcessByPath(ByVal path As String)
+        Try
+            Dim searcher As New ManagementObjectSearcher("SELECT * FROM Win32_process WHERE ExecutablePath = '" _
+                                                         + path.Replace("\", "\\") + "'")
+            For Each result As ManagementObject In searcher.Get()
+                Dim PID As Integer = CInt(result.Item("ProcessId"))
+                Dim P As Process = Process.GetProcessById(PID)
+                P.Kill()
+            Next
+        Catch ex As Exception
+            ' handle exception
+        End Try
+    End Sub
+
+    Sub TimerCount()
+        Dim o As Integer = lblSecond.Text
+
+        Dim Count As Integer = 0
+        Dim x As Integer = lblSecond.Text
+
+        While True
+            x = x - 1
+            lblSecond.Text = x
+            lblStatus.Text = "Wait to Kill"
+            If x = 0 Then
+                x = o
+
+                Count = Count + 1
+
+                lblStatus.Text = "Process Killed wating 10 second to run Again"
+
+                KillProcessByPath(txtFilePath.Text)
+
+
+                Threading.Thread.Sleep(10000)
+
+
+                RunPy()
+
+                lblRunTime.Text = Count
+            End If
+
+            Threading.Thread.Sleep(1000)
+        End While
+
+    End Sub
+
+    Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        th.Abort()
+        lblSecond.Text = "0"
+        btnStop.Enabled = False
+        btnStart.Enabled = True
+        txtInterval.Enabled = True
+        CheckBox1.Enabled = True
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim openFileDialog1 As New OpenFileDialog()
+
+        openFileDialog1.InitialDirectory = "C:\Windows"
+        '  openFileDialog1.Filter = "PyEXE (*.exe)|*.exe|All Files (*.*)|*.*"
+        openFileDialog1.Filter = "PyEXE (*.exe)|*.exe"
+        openFileDialog1.FilterIndex = 2
+        openFileDialog1.RestoreDirectory = True
+
+        If openFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            Dim selectedFilePath As String = openFileDialog1.FileName
+            txtFilePath.Text = selectedFilePath
+            ' Do something with the selected file path
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim openFileDialog1 As New OpenFileDialog()
+
+        openFileDialog1.InitialDirectory = "C:\"
+        '  openFileDialog1.Filter = "PyEXE (*.exe)|*.exe|All Files (*.*)|*.*"
+        openFileDialog1.Filter = "PyFile (*.py)|*.py"
+        openFileDialog1.FilterIndex = 2
+        openFileDialog1.RestoreDirectory = True
+
+        If openFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            Dim selectedFilePath As String = openFileDialog1.FileName
+            txtPyFile.Text = selectedFilePath
+            ' Do something with the selected file path
+        End If
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim openFileDialog1 As New OpenFileDialog()
+
+        openFileDialog1.InitialDirectory = "C:\Program Files (x86)\"
+        '  openFileDialog1.Filter = "PyEXE (*.exe)|*.exe|All Files (*.*)|*.*"
+        openFileDialog1.Filter = "PythonEXE (*.exe)|*.exe"
+        openFileDialog1.FilterIndex = 2
+        openFileDialog1.RestoreDirectory = True
+
+        If openFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            Dim selectedFilePath As String = openFileDialog1.FileName
+            txtPythonFile.Text = selectedFilePath
+            ' Do something with the selected file path
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            Label8.Text = "Minutes"
+        Else
+            Label8.Text = "Second"
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        KillProcessByPath(txtFilePath.Text)
+
+    End Sub
+End Class
